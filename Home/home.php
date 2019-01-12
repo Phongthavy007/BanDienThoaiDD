5034ef2e6a078589c72d5416c2928f499758c4f9
Phongthavy007 <46478738+Phongthavy007@users.noreply.github.com>
2019-01-12 18:41:22 +0700

    Design Trang chủ
    Design Trang chủ

---
+++ Home/home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Trang chủ</title>
+<link rel="stylesheet" href="../CSS/style.css" />
+<link rel="icon" type="image/png" href="../anh/logo_shop.ico"/>
+</head>
+
+<body>
+<div id="container">
+    <div class="header">
+    	<img src="../anh/banner.png" />
+    </div><!--Kết thúc header-->
+    
+    <div class="menu">
+    	<h3>Menu</h3>
+    </div><!--Kết thúc menu-->
+    
+    <div class="left">
+    	<h3>Left</h3>
+    </div><!--Kết thúc left-->
+    
+    <div class="content">
+    	<h3>content</h3>
+    </div><!--Kết thúc content-->
+    
+    <div class="clear"></div>
+    <div class="footer">
+    	<h3>Footer</h3>
+    </div><!--Kết thúc footer-->
+</div><!--Kết thúc container-->
+</body>
+</html>
