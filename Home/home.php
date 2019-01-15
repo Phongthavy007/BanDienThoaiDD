0ab0fcf12a1e3e0c147ef38e6210aacfb8256a7a
Phongthavy007 <46478738+Phongthavy007@users.noreply.github.com>
2019-01-16 00:09:41 +0700

    Design trang chủ với Bootstrap
    Design trang chủ với Bootstrap

--- Home/home.php
+++ Home/home.php
@@ -2,33 +2,181 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Trang chủ</title>
-<link rel="stylesheet" href="../CSS/style.css" />
+<!--<link rel="stylesheet" href="../CSS/style.css" />-->
 <link rel="icon" type="image/png" href="../anh/logo_shop.ico"/>
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.css" type="text/css"/>
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" type="text/css"/>
+<style type="text/css">
+.header{width:100%; height:225px; margin-top:-10px; font-family:"Times New Roman", Times, serif;}
+.header>img{width:100%; height:225px;}
+</style>
 </head>
 
 <body>
-<div id="container">
-    <div class="header">
-    	<img src="../anh/banner.png" />
-    </div><!--Kết thúc header-->
-    
-    <div class="menu">
-    	<h3>Menu</h3>
-    </div><!--Kết thúc menu-->
-    
-    <div class="left">
-    	<h3>Left</h3>
-    </div><!--Kết thúc left-->
-    
-    <div class="content">
-    	<h3>content</h3>
-    </div><!--Kết thúc content-->
-    
-    <div class="clear"></div>
-    <div class="footer">
-    	<h3>Footer</h3>
-    </div><!--Kết thúc footer-->
-</div><!--Kết thúc container-->
+<nav class="navbar navbar-collapse" style="border-color:#09F; font-size:18px; font-family:'Times New Roman', Times, serif;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+      	<img style="margin-top:-10px;" alt="Brand" src="../anh/logo_shop.ico" width="40" height="40">
+      </a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="#">Trang chủ</a></li>
+      <li><a href="#">Giới thiệu</a></li>
+      <!--<li><a href="#">Sản phẩm</a></li>-->
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Sản phẩm
+      <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li role="separator" class="divider"></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li role="separator" class="divider"></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Liên hệ</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Tìm kiếm...">
+        </div>
+        <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span>Tìm</button>
+      </form>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+	<div class="bg-warning">
+    	<div class="header">
+        	<img src="../anh/banner.png" />
+        </div>
+        <div class="row" style="margin-top:10px;">
+          <div class="col-xs-6 col-md-3">
+          	<div class="panel panel-default">
+            	<div style="background-color:#09F; color:#FFF;" class="panel-heading">Loài Sản Phẩm</div>
+                <div class="panel-body">
+                	<div class="list-group">
+                      <button type="button" class="list-group-item">iphone</button>
+                      <button type="button" class="list-group-item">Samsung</button>
+                      <button type="button" class="list-group-item">Nokia</button>
+                      <button type="button" class="list-group-item">Huawei</button>
+                    </div>
+                </div>
+                <div style="background-color:#09F; color:#FFF;" class="panel-heading">Khuyển mãi</div>
+                <div class="panel-body">
+                	<div class="list-group">
+                      <button type="button" class="list-group-item">iphone</button>
+                      <button type="button" class="list-group-item">Samsung</button>
+                      <button type="button" class="list-group-item">Nokia</button>
+                      <button type="button" class="list-group-item">Huawei</button>
+                    </div>
+                </div>
+            </div>
+          </div>
+          
+          <div class="col-xs-12 col-sm-6 col-md-9">
+          	<div class="panel panel-default">
+            <div style="background-color:#09F; color:#FFF;" class="panel-heading">Sản Phẩm Mới</div>
+                <div class="panel-body">
+                
+                	<div class="row">
+                      <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                          <img src="../anh/10.jpg" alt="...">
+                          <div class="caption">
+                            <h3>Thumbnail label</h3>
+                            <p>...</p>
+                            <p align="center"><a href="#" class="btn btn-primary" role="button">Xem chỉ tiết</a></p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                          <img src="../anh/10.jpg" alt="...">
+                          <div class="caption">
+                            <h3>Thumbnail label</h3>
+                            <p>...</p>
+                            <p align="center"><a href="#" class="btn btn-primary" role="button">Xem chỉ tiết</a></p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                          <img src="../anh/10.jpg" alt="...">
+                          <div class="caption">
+                            <h3>Thumbnail label</h3>
+                            <p>...</p>
+                            <p align="center"><a href="#" class="btn btn-primary" role="button">Xem chỉ tiết</a></p>
+                          </div>
+                        </div>
+                      </div>
+                   </div>
+            
+            	<div style="background-color:#09F; color:#FFF; margin:auto -13px;" class="panel-heading">Sản Phẩm</div>
+                <div class="panel-body">
+                
+                	<div class="row">
+                      <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                          <img src="../anh/10.jpg" alt="...">
+                          <div class="caption">
+                            <h3>Thumbnail label</h3>
+                            <p>...</p>
+                            <p align="center"><a href="#" class="btn btn-primary" role="button">Xem chỉ tiết</a></p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                          <img src="../anh/10.jpg" alt="...">
+                          <div class="caption">
+                            <h3>Thumbnail label</h3>
+                            <p>...</p>
+                            <p align="center"><a href="#" class="btn btn-primary" role="button">Xem chỉ tiết</a></p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                          <img src="../anh/10.jpg" alt="...">
+                          <div class="caption">
+                            <h3>Thumbnail label</h3>
+                            <p>...</p>
+                            <p align="center"><a href="#" class="btn btn-primary" role="button">Xem chỉ tiết</a></p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                          <img src="../anh/10.jpg" alt="...">
+                          <div class="caption">
+                            <h3>Thumbnail label</h3>
+                            <p>...</p>
+                            <p align="center"><a href="#" class="btn btn-primary" role="button">Xem chỉ tiết</a></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div><!--Kết thúc row-->
+                    
+                </div>
+            </div>
+          </div>
+          
+        </div><!--Kết thúc row-->
+    </div><!--Kết thúc background-->
+</div><!--Kết thúc Container-->
+
+<footer class="panel-footer text-center" >
+  <p>Copyright@HTU</p>
+</footer>
+<script src="../bootstrap/js/jquery-3.2.1.min.js"></script>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
